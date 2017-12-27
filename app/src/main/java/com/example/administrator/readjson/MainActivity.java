a3b0b001ba71e06fb75676db92149d2b02ee813a
gdmec <admin@gdmec.cn>
2017-12-27 15:19:30 +0800

    改

--- app/src/main/java/com/example/administrator/readjson/MainActivity.java
+++ app/src/main/java/com/example/administrator/readjson/MainActivity.java
@@ -1,12 +1,16 @@
 package com.example.administrator.readjson;
 
 import android.os.Handler;
+import android.os.Looper;
 import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AutoCompleteTextView;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
@@ -15,45 +19,33 @@ import org.w3c.dom.Text;
 import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
 
 public class MainActivity extends AppCompatActivity {
+    private String cityname="广州";
+    HttpURLConnection httpConn = null;
+    InputStream din =null;
     TextView tv_test;
     Button btn_test;
+    EditText et_input;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        tv_test = (TextView)findViewById(R.id.tv_test);
-        btn_test = (Button)findViewById(R.id.btn_test);
-
+        setTitle("天气预报JSON");
+        et_input = (EditText) findViewById(R.id.et_city);
+        btn_test = (Button) findViewById(R.id.btn_test);
+        tv_test = (TextView) findViewById(R.id.tv_test);
         btn_test.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String r = readWeatherinf();
-                try {
-                    JSONObject jobj = new JSONObject(r);
-                    JSONObject weather = jobj.getJSONObject("data");
-                    StringBuffer wbf = new StringBuffer();
-                    wbf.append("所选城市："+weather.getString("city")+"\n");
-                    wbf.append("温度："+weather.getString("wendu")+"\n");
-                    wbf.append("天气提示："+weather.getString("ganmao")+"\n");
-
-                    JSONArray jary = weather.getJSONArray("forecast");
-                    for(int i=0;i<jary.length();i++){
-                        JSONObject pobj = (JSONObject)jary.opt(i);
-                        wbf.append("日期："+pobj.getString("date")+"\n");
-                        wbf.append("高温："+pobj.getString("high")+"\n");
-                        wbf.append("低温："+pobj.getString("low")+"\n");
-                        wbf.append("风向："+pobj.getString("fengxiang")+"\n");
-                        wbf.append("风力："+pobj.getString("fengli")+"\n");
-                        wbf.append("天气："+pobj.getString("type")+"\n");
-
-                    }
-                    tv_test.setText(wbf.toString());
-                }catch (Exception ex){
-                    ex.printStackTrace();
-                }
-
+                tv_test.setText("");
+                cityname = et_input.getText().toString();
+                Toast.makeText(MainActivity.this,"正在查询天气...",Toast.LENGTH_LONG).show();
+                GetJson gd = new GetJson(cityname);
+                gd.start();
             }
         });
     }
@@ -61,7 +53,7 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public void handleMessage(Message msg) {
             switch (msg.what){
-                case 123:
+                case 1:
                     showData((String)msg.obj);
                     break;
             }
@@ -69,25 +61,72 @@ public class MainActivity extends AppCompatActivity {
         }
     };
     private  void showData(String jData){
-        tv_test.setText(jData);
-        //这里我直接显示json数据，没解析。解析的方法，请参考教材或网上相应的代码
+        try {
+            JSONObject jobj = new JSONObject(jData);
+            JSONObject weather = jobj.getJSONObject("data");
+            StringBuffer wbf = new StringBuffer();
+            wbf.append("当前温度："+weather.getString("wendu")+"℃"+"\n");
+            wbf.append("天气提示："+weather.getString("ganmao")+"\n");
+            JSONArray jary = weather.getJSONArray("forecast");
+            for(int i=0;i<jary.length();i++){
+                JSONObject pobj = (JSONObject)jary.opt(i);
+                wbf.append("日期："+pobj.getString("date")+"\n");
+                wbf.append("最高温："+pobj.getString("high")+"\n");
+                wbf.append("最低温："+pobj.getString("low")+"\n");
+                wbf.append("风向："+pobj.getString("fengxiang"));
+                wbf.append("风力："+pobj.getString("fengli")+"\n");
+                wbf.append("天气："+pobj.getString("type")+"\n");
+            }
+            tv_test.setText(wbf.toString());
+        }catch (Exception ex){
+            ex.printStackTrace();
+        }
 
     }
-    private  String readWeatherinf(){
-        String winf = "";
-        InputStream ins = getResources().openRawResource(R.raw.ww);
-        InputStreamReader insr = new InputStreamReader(ins);
-        BufferedReader br = new BufferedReader(insr);
-        StringBuffer buff = new StringBuffer();
-        String line = null;
-        try {
-            while ((line = br.readLine()) != null) {
-                 buff.append(line);
+    class GetJson extends Thread{
+        private String urlstr =  "http://wthrcdn.etouch.cn/weather_mini?city=";
+        public GetJson(String cityname){
+            try{
+                urlstr = urlstr+ URLEncoder.encode(cityname,"UTF-8");
+            }catch (Exception ee){
+                ee.printStackTrace();
             }
-        }catch (Exception e){
-            e.printStackTrace();
         }
-        winf = buff.toString();
-        return  winf;
+        @Override
+        public void run() {
+            try {
+                URL url = new URL(urlstr);
+                httpConn = (HttpURLConnection)url.openConnection();
+                httpConn.setRequestMethod("GET");
+                din = httpConn.getInputStream();
+                InputStreamReader in = new InputStreamReader(din);
+                BufferedReader buffer = new BufferedReader(in);
+                StringBuffer sbf = new StringBuffer();
+                String line = null;
+                while( (line=buffer.readLine())!=null) {
+                    sbf.append(line);
+                }
+                Message msg = new Message();
+                msg.obj = sbf.toString();
+                msg.what = 1;
+                handler.sendMessage(msg);
+                Looper.prepare();
+                Toast.makeText(MainActivity.this,"获取数据成功",Toast.LENGTH_LONG).show();
+                Looper.loop();
+            }catch (Exception ee){
+                Looper.prepare();
+                Toast.makeText(MainActivity.this,"获取数据失败，网络连接失败或输入有误",Toast.LENGTH_LONG).show();
+                Looper.loop();
+                ee.printStackTrace();
+            }finally {
+                try{
+                    httpConn.disconnect();
+                    din.close();
+
+                }catch (Exception ee){
+                    ee.printStackTrace();
+                }
+            }
+        }
     }
 }
